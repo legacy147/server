20de2e24197b2a5eacebc8a4a00638efcdca50ff
legacy147 <odehinalegwu80@gmail.com>
2024-10-31 16:08:16 +0100

    addedconfiguration

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from food import views
 from account import views as accountview
 
@@ -59,4 +59,4 @@ urlpatterns = [
     
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
